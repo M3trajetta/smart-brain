64584beced88c1f751e5992adf38b31562872614
Mohammed Tantawy <mtantawy.mido@gmail.com>
2019-12-12 00:29:31 +1100

    Added skeleton of the application

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className="white f3">{`YO, your current entry count is...`}</div>
+      <div className="white f1">#4</div>
+    </div>
+  );
+};
+
+export default Rank;
